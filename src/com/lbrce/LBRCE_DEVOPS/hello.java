2a11a0a8e2badeb343e3b6ad6ece9a2e07d836e5
AdmiN <AdmiN@ITWS0031>
2024-10-22 10:09:28 +0530

    Merge branch 'master' of https://github.com/srinivas111-ui/LBRCE_DEVOPS

--- src/com/lbrce/LBRCE_DEVOPS/hello.java
+++ src/com/lbrce/LBRCE_DEVOPS/hello.java
@@ -4,6 +4,7 @@ public class hello {
 
 	public static void main(String[] args) {
 		System.out.println("Updated after adding");
+		System.out.println("This a update");
 		// TODO Auto-generated method stub
 
 	}
